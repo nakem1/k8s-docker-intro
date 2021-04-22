3250d80c84b7b82841ad20492a358315044b74a6
nakem1 <annaberdiev00@icloud.com>
2021-04-22 20:09:21 +0300

    done

--- setup.sh
+++ setup.sh
@@ -11,29 +11,29 @@ docker pull metallb/controller:v0.8.2
 minikube addons enable dashboard
 minikube addons enable metallb
 
-#kubectl apply -f metallb.yaml
+kubectl apply -f metallb.yaml
 #
-#docker build -t nginx_image ./srcs/nginx/
-#kubectl apply -f srcs/nginx/nginx.yaml
-#
-#docker build -t phpmyadmin_image ./srcs/phpmyadmin/
-#kubectl apply -f srcs/phpmyadmin/php.yaml
-#
-#docker build -t wordpress_image ./srcs/wordpress/
-#kubectl apply -f srcs/wordpress/wp.yaml
-#
-#docker build -t mysql_image ./srcs/mysql/
-#kubectl apply -f srcs/mysql/mysql.yaml
-#
-##docker build -t influxdb_image ./srcs/influxdb/
-##kubectl apply -f srcs/influxdb/influxdb.yaml
-#
-##docker build -t grafana_image ./srcs/grafana/
-##kubectl apply -f srcs/grafana/grafana.yaml
-#
+docker build -t nginx_image ./srcs/nginx/
+kubectl apply -f srcs/nginx/nginx.yaml
+
+docker build -t phpmyadmin_image ./srcs/phpmyadmin/
+kubectl apply -f srcs/phpmyadmin/php.yaml
+
+docker build -t wordpress_image ./srcs/wordpress/
+kubectl apply -f srcs/wordpress/wp.yaml
+
+docker build -t mysql_image ./srcs/mysql/
+kubectl apply -f srcs/mysql/mysql.yaml
+
+docker build -t influxdb_image ./srcs/influxdb/
+kubectl apply -f srcs/influxdb/influxdb.yaml
+
+docker build -t grafana_image ./srcs/grafana/
+kubectl apply -f srcs/grafana/grafana.yaml
+
 docker build -t ftps_image ./srcs/ftps/
 kubectl apply -f srcs/ftps/ftps.yaml
 #
 ##ssh-keygen -R 192.168.99.115
 #
-#minikube dashboard &
+minikube dashboard &
